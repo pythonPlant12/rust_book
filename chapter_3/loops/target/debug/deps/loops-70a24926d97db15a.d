3eab256ffda64a423a2d1598ccafd710dd10213f
pythonPlant12 <pythonplant12@gmail.com>
2024-10-14 19:28:06 +0200

    - Adding new chapter 5 - Renaming previous chapters

--- chapter_3/loops/target/debug/deps/loops-70a24926d97db15a.d
+++
@@ -1,5 +0,0 @@
-/Users/nikitalutsai/Programming/study/rust/rust_book/chapter_3/loops/target/debug/deps/loops-70a24926d97db15a: src/main.rs
-
-/Users/nikitalutsai/Programming/study/rust/rust_book/chapter_3/loops/target/debug/deps/loops-70a24926d97db15a.d: src/main.rs
-
-src/main.rs:
--- chapter_3/loops/target/debug/loops.d
+++
@@ -1,1 +0,0 @@
-/Users/nikitalutsai/Programming/study/rust/rust_book/chapter_3/loops/target/debug/loops: /Users/nikitalutsai/Programming/study/rust/rust_book/chapter_3/loops/src/main.rs
